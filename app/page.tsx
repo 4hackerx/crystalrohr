bfdba8c412cff14adfeeaea14612b76bdbe08c72
yashika <mehndirattayashika5@gmail.com>
2024-08-30 20:30:45 +0530

    chore: added Role page

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@ import { Play, Eye, Users, Zap } from 'lucide-react';
 import {BeamBack} from "@/components/atoms/BeamBack";
 import { CardSpotlight } from "@/components/atoms/CardSpotlight";
 import { TextGenerateEffect } from "@/components/atoms/TextGenerate";
+import Link from "next/link";
 const page = () => {
   return (
-    <div className="h-screen  bg-gradient-to-r from-black to-[#550EFB]">
+    <div className="h-screen  bg-gradient-to-r from-black to-[#550EFB] overflow-x-hidden">
       <main className="container w-screen">
           <section className="text-center h-screen flex items-center justify-center gap-3 flex-col">
             <h2 className="text-6xl font-bold text-white mb-4">
               Bring Clarity to Online Videos
               </h2>
             <p className="text-xl text-gray-400 mb-8">Make your content accessible to millions of visually impaired users</p>
-            <a href="#cta" className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition duration-300">Get Started</a>
+            <Link href="/Role" className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition duration-300">Get Started</Link>
           </section>
       </main>
     </div>
