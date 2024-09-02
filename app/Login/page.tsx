914ce18663e569125fb1064d9decb4b61e76f651
yashika <mehndirattayashika5@gmail.com>
2024-09-02 13:38:52 +0530

    feat: fixed build issues-2

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,10 +1,10 @@
-import LoginButton from '@/components/molecules/Login-button'
+// import LoginButton from '@/components/molecules/Login-button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <LoginButton/>
+      {/* <LoginButton/> */}
     </div>
   )
 }
