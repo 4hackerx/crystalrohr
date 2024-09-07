3dac3d959456bb411ba0924f31d907692d9e42fc
kelvinpraises <kelvinpraises@gmail.com>
2024-09-07 12:12:11 +0100

    chore: prettify

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/serviceworker/:path*',
-            destination: '/:path*', 
-            permanent: true,
-          },
-        ];
+  async redirects() {
+    return [
+      {
+        source: "/serviceworker/:path*",
+        destination: "/:path*",
+        permanent: true,
       },
-    
+    ];
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
