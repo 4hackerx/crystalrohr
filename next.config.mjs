5ff3bf61df9378765d9b3769a566ab9ff4ec22df
kelvinpraises <kelvinpraises@gmail.com>
2024-09-07 20:08:54 +0100

    fix: xmtp issues and connect wallet

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@xmtp/user-preferences-bindings-wasm"],
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
   async redirects() {
     return [
       {
